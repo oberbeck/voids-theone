90b8c6ec21c2b2eb207b49b8f93bbcd2df3eae5a
oberbeck <20164654+oberbeck@users.noreply.github.com>
2024-02-18 13:45:44 +0100

    generate forecast resource
    execute `yarn nx generate @nestjs/schematics:resource --name=forecasts --sourceRoot=backend/src/app --no-interactive`

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
+import { ForecastsModule } from './forecasts/forecasts.module';
 
 @Module({
-  imports: [WeatherModule],
+  imports: [WeatherModule, ForecastsModule],
   controllers: [AppController],
   providers: [AppService],
 })
