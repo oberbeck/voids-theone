8830b7e890b6c77078a9ac71d13cff517e879fdf
oberbeck <20164654+oberbeck@users.noreply.github.com>
2024-02-18 12:29:01 +0100

    add header component

---
+++ frontend/src/app/components/header.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+export default function Header({
+  showHomeButton,
+}: {
+  showHomeButton?: boolean;
+}) {
+  return (
+    <div className="navbar bg-base-100 gap-2">
+      <div className="flex-none text-4xl">🕶️</div>
+      <div className="flex-1">ONEGLASS.io</div>
+      <div className="flex-none flex gap-2">
+        {showHomeButton !== false && (
+          <Link href="/" className="btn">
+            back
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+}
